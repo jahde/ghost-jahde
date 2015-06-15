a1ca4ad872585c44091b41e45a19a2a126af3d13
jahde <jahde.hiywan@gmail.com>
2015-06-15 09:16:26 -0400

    change to code jahde

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://ghost-jahde.herokuapp.com',
+        url: 'http://code.jahde.co',
         fileStorage: false,
         mail: {
             transport: 'SMTP',
